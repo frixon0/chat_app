3812d01b2346cd7c00a300cf87bdab64658a1803
Tanmay Sharma <tanmay7575sharma@gmail.com>
2026-05-14 13:07:36 +0530

    completed create room endpoint

---
+++ next_logic/components/Create.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { customAlphabet } from "nanoid"
+import { useState } from "react"
+import { Socketconnect } from "./Socketconnect"
+
+type Props={
+    open:boolean,
+    onclose:()=>void
+}
+
+export const Create = ({open,onclose}:Props) => {
+    const[id,setid] = useState("");
+    const alphabet = '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+    function genid() : string {
+        const nanoid = customAlphabet(alphabet, 4);
+        return `${nanoid()}-${nanoid()}`;
+    }
+    if(!open)  return null;
+    if(id) return <Socketconnect roomId={id}/>
+    
+       
+    
+    const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if ((e.target as HTMLDivElement).id === 'backdrop') {
+      onclose();
+    }
+    }
+    
+  return (
+    <div  id="backdrop"
+      onClick={handleBackdropClick}
+    className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+        
+        <button
+        onClick={() => {
+            const newid = genid;
+            setid(newid);
+        }
+        }  
+        className="cursor-pointer border-2 rounded-md mr-2 ml-2 pl-2 pr-2 hover:text-green-600">Enter</button>
+    </div>
+  )
+}
+
--- next_logic/components/CreateRoom.tsx
+++ next_logic/components/CreateRoom.tsx
@@ -1,9 +1,20 @@
 "use client"
-const createhandle =()=>{
- 
-}
+
+import { useState } from "react"
+import { Create } from "./Create";
+
 export const CreateRoom = () => {
+    const [open,setopen] =useState(false);
+    const createhandle=()=>{
+        setopen(true);
+    }
   return (
-    <div><button onClick={createhandle}className="cursor-pointer border-2 rounded-md mr-2 ml-2 pl-2 pr-2 hover:text-green-600">Create Room</button></div>
+    <div><button onClick={createhandle}className="cursor-pointer border-2 rounded-md mr-2 ml-2 pl-2 pr-2 hover:text-green-600">Create Room</button>
+    {open&&(
+        <Create
+        open ={open}
+        onclose={()=>setopen(false)}/>
+    )}
+    </div>
   )
 }
